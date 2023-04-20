1b4c01a307569f4b95dc270c1d177c3613f874f8
Ricardo Alves <dev.rical21@gmail.com>
2023-04-20 12:47:39 -0300

    05. TSC e StrictNullChecks

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -13,9 +13,11 @@ export class NegociacaoController {
   private mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
-    this.inputData = document.querySelector("#data");
-    this.inputQuantidade = document.querySelector("#quantidade");
-    this.inputValor = document.querySelector("#valor");
+    this.inputData = <HTMLInputElement>document.querySelector("#data");
+    this.inputQuantidade = document.querySelector(
+      "#quantidade"
+    ) as HTMLInputElement;
+    this.inputValor = document.querySelector("#valor") as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
 
@@ -25,10 +27,12 @@ export class NegociacaoController {
       this.inputQuantidade.value,
       this.inputValor.value
     );
+
     if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
       return;
     }
+
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -7,13 +7,10 @@ export class Negociacao {
     get volume() {
         return this.quantidade * this.valor;
     }
-    // defensivamente criamos uma referencia de data baseada na propriedade _data e assim,
-    // o método get retorna a variável e não a propriedade para assim defendermos nossa propriedade _data
     get data() {
         const data = new Date(this._data.getTime());
         return data;
     }
-    // todo método static pode ser chamado diretamente pela própria classe | invocando a classe. Exemplo: Classe.metodo()
     static criaDe(dataString, quantidadeString, valorString) {
         const expRegEx = /-/g;
         const date = new Date(dataString.replace(expRegEx, ","));
